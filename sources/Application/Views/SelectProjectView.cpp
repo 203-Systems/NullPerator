5e8b6476c66d0274188f0941c84b5eb527344547
Nengzhuo Cai <Null@203.io>
2026-08-23 21:04:26 +0800

    fix(project): restore standard delete confirmation

--- sources/Application/Views/SelectProjectView.cpp
+++ sources/Application/Views/SelectProjectView.cpp
@@ -8,150 +8,13 @@
  */
 
 #include "SelectProjectView.h"
-#include "Application/AppWindow.h"
 #include "Application/Persistency/PersistencyService.h"
-#include "Application/Utils/DrawUtils.h"
 #include "Application/Views/ModalDialogs/MessageBox.h"
 #include "BaseClasses/ViewEvent.h"
-#include "Foundation/Constants/SpecialCharacters.h"
-#include "System/System/System.h"
-#include <new>
 
 #define LIST_PAGE_SIZE (SCREEN_HEIGHT - 4)
 #define LIST_WIDTH 26
 #define INVALID_PROJECT_NAME "INVALID NAME"
-#define DELETE_HOLD_MASK (EPBM_ALT | EPBM_PLAY | EPBM_EDIT)
-#define DELETE_HOLD_DURATION_MS 2000
-
-class DeleteProjectConfirmModal : public ModalView {
-public:
-  static DeleteProjectConfirmModal *Create(View &view, const char *projectName);
-  virtual ~DeleteProjectConfirmModal();
-  virtual void Destroy() override;
-  virtual void DrawView() override;
-  virtual void OnPlayerUpdate(PlayerEventType, unsigned int) override {}
-  virtual void OnFocus() override {}
-  virtual void ProcessButtonMask(unsigned short mask, bool pressed) override;
-  virtual void AnimationUpdate() override;
-
-private:
-  DeleteProjectConfirmModal(View &view, const char *projectName);
-  void UpdateProgress_();
-  static bool inUse_;
-  static void *storage_;
-  etl::string<MAX_PROJECT_NAME_LENGTH + 12> projectLine_;
-  unsigned short currentMask_ = 0;
-  unsigned long holdStartMs_ = 0;
-  uint16_t holdProgressMs_ = 0;
-  bool holdingCombo_ = false;
-};
-
-bool DeleteProjectConfirmModal::inUse_ = false;
-alignas(
-    DeleteProjectConfirmModal) static unsigned char DeleteProjectConfirmModalStorage
-    [sizeof(DeleteProjectConfirmModal)];
-void *DeleteProjectConfirmModal::storage_ = DeleteProjectConfirmModalStorage;
-
-DeleteProjectConfirmModal *DeleteProjectConfirmModal::Create(View &view,
-                                                             const char *name) {
-  if (inUse_) {
-    auto *existing = reinterpret_cast<DeleteProjectConfirmModal *>(storage_);
-    existing->~DeleteProjectConfirmModal();
-    inUse_ = false;
-  }
-  inUse_ = true;
-  return new (storage_) DeleteProjectConfirmModal(view, name);
-}
-
-DeleteProjectConfirmModal::DeleteProjectConfirmModal(View &view,
-                                                     const char *projectName)
-    : ModalView(view), projectLine_("Delete \"") {
-  projectLine_.append(projectName);
-  projectLine_.append("\"");
-}
-
-DeleteProjectConfirmModal::~DeleteProjectConfirmModal() {}
-
-void DeleteProjectConfirmModal::Destroy() {
-  this->~DeleteProjectConfirmModal();
-  inUse_ = false;
-}
-
-void DeleteProjectConfirmModal::UpdateProgress_() {
-  const bool comboHeld = (currentMask_ & DELETE_HOLD_MASK) == DELETE_HOLD_MASK;
-  const unsigned long now = System::GetInstance()->GetClock();
-
-  if (!comboHeld) {
-    if (holdingCombo_ || (holdProgressMs_ != 0)) {
-      holdingCombo_ = false;
-      holdProgressMs_ = 0;
-      isDirty_ = true;
-      static_cast<AppWindow &>(w_).SetDirty();
-    }
-    return;
-  }
-
-  if (!holdingCombo_) {
-    holdingCombo_ = true;
-    holdStartMs_ = now;
-    holdProgressMs_ = 0;
-    isDirty_ = true;
-    static_cast<AppWindow &>(w_).SetDirty();
-    return;
-  }
-
-  unsigned long elapsed = now - holdStartMs_;
-  if (elapsed > DELETE_HOLD_DURATION_MS) {
-    elapsed = DELETE_HOLD_DURATION_MS;
-  }
-
-  if (holdProgressMs_ != elapsed) {
-    holdProgressMs_ = elapsed;
-    isDirty_ = true;
-    static_cast<AppWindow &>(w_).SetDirty();
-  }
-
-  if (holdProgressMs_ >= DELETE_HOLD_DURATION_MS) {
-    EndModal(MBL_YES);
-  }
-}
-
-void DeleteProjectConfirmModal::AnimationUpdate() { UpdateProgress_(); }
-
-void DeleteProjectConfirmModal::ProcessButtonMask(unsigned short mask,
-                                                  bool pressed) {
-  currentMask_ = mask;
-
-  if (pressed && (mask & EPBM_ENTER)) {
-    EndModal(MBL_CANCEL);
-    return;
-  }
-
-  UpdateProgress_();
-}
-
-void DeleteProjectConfirmModal::DrawView() {
-  SetWindow(26, 6);
-
-  GUITextProperties props;
-  SetColor(CD_NORMAL);
-  props.invert_ = false;
-
-  const int projectLineX = (26 - projectLine_.size()) / 2;
-  DrawString(projectLineX, 0, projectLine_.c_str(), props);
-  DrawString(0, 1, "Press & hold ALT+PLAY+EDIT", props);
-
-  if (holdingCombo_ || holdProgressMs_ > 0) {
-    progressBar_t progressBar;
-    fillProgressBar(holdProgressMs_, DELETE_HOLD_DURATION_MS, &progressBar);
-    DrawString((26 - 12) / 2, 3, progressBar, props);
-  }
-
-  const char *cancelButton = "[ Cancel ]";
-  SetColor(CD_HILITE2);
-  props.invert_ = true;
-  DrawString((26 - strlen(cancelButton)) / 2, 5, cancelButton, props);
-}
 
 static void LoadProjectCallback(View &v, ModalView &dialog) {
   if (dialog.GetReturnCode() == MBL_YES) {
@@ -465,7 +328,8 @@ void SelectProjectView::AttemptDeletingSelectedProject() {
   char selected[MAX_PROJECT_NAME_LENGTH + 1];
   getHighlightedProjectName(selected);
 
-  ModalView *mb = DeleteProjectConfirmModal::Create(*this, selected);
+  MessageBox *mb = MessageBox::Create(*this, "Delete selected project?",
+                                      selected, MBBF_YES | MBBF_NO);
   DoModal(mb, ModalViewCallback::create<&DeleteProjectCallback>());
 }
 
